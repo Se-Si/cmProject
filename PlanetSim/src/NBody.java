6be349543fc3e5e5f91ec432e48237e6bc7d62b5
SilverSythe <sebvdb@me.com>
2015-11-15 15:52:53 +0000

    CHANGES: -Changed 1s to 1.0 to avoid int tyecasting issues, just to be safe -Changed potential energy calculation to include a negative sign -Various changes to the main loop to incorporate necessary methods such as leapPosition and leapVelocity, not tested yet
    ADDITIONS:
    -Implemented totalEnergy(), I hope the calculation is correct
    -Implemented leapPositions and leapVelocities
    -Added a convenience method to return an arrayList of forces directly, taking in the list of particles, mostly for convenience and less clutter in the main loop

--- PlanetSim/src/NBody.java
+++ PlanetSim/src/NBody.java
@@ -8,9 +8,6 @@ public class NBody {
 	// Gravitational Constant
 	static double g = 1.0;
 
-	//
-
-
 	public static void main (String[] argv) throws IOException {
 
 		// Number of timesteps
@@ -21,95 +18,32 @@ public class NBody {
 		double t = 0;
 
 		//Read Planet info from SolarParticles.dat into 'particles' list
-
 		ArrayList<Particle3D> particles = Particle3D.readFile("SolarParticles.dat");
-		int particleCount = (particles.size() - 1);    	
-		int tempParticleCount1;
-		int tempParticleCount2;
+
+
+		// Current forces at time t acting on all of the particles
+		ArrayList<Vector3D> currentForces;
+		// New forces at time t+dt acting on all of the particles
+		ArrayList<Vector3D> newForces;
 
 
 		//VERLET START
 
+		// Compute initial forces
+		currentForces = totalInteractionForces(particles);
 
-		//////////////////////////
+		for(int n=0;n<numstep;n++) {
+			// Leap all the particle positions
+			leapPositions(particles, currentForces, dt);
 
-		//Force calculations, don't need to beware double-counting
-		//For Loop should work, haven't had time to test yet
-		//create gforcecurrent list and populate;
-		//list structure:
-		// 0-1, ... 0-n, 1-0, 1-2, ..., n-(n-1)
-			
-		ArrayList<Vector3D> gforcecurrent = new ArrayList<>();
-		
-		tempParticleCount1 = 0;
-		
-		for (tempParticleCount1 = 0; tempParticleCount1 > particleCount; tempParticleCount1 += 1){
-			for (tempParticleCount2 = 0; tempParticleCount2 > particleCount; tempParticleCount2 += 1){
-				if (tempParticleCount1 != tempParticleCount2){
-				
-					gforcecurrent.add(gForce(particles.get(tempParticleCount1),particles.get(tempParticleCount2)));			
-			}	
+			// Calculate new forces, with particles at time t+dt
+			newForces = totalInteractionForces(particles);
+
+			// Leap the velocities from the average of the forces
+			leapVelocities(particles, elementAverage(currentForces, newForces), dt);
 		}
-		}
-
-		///////////////////////
-
-		//STOPPED WORKING AFTER FOR LOOP FIX, NEEDS HELP
-		//(Also realised it wasn't working in the first place, but oh well)
-		//Update Position of particles list based upon gforcecurrent list
-		//sum gforcecurrent for each object, apply to object
-
-		Vector3D tempGForce = new Vector3D (0.0,0.0,0.0);
-		tempParticleCount1 = 0;
-		tempParticleCount2 = 0;
-		int nloop = 0;
-		while (tempParticleCount1 <= particleCount){
-			tempGForce.setX(0.0);
-			tempGForce.setY(0.0);
-			tempGForce.setZ(0.0);
-			
-			while (tempParticleCount2 <= (particleCount*(particleCount-1))){
-
-				tempGForce = Vector3D.add(tempGForce,gforcecurrent.get(tempParticleCount2));
-				tempParticleCount2 += 1;
-
-			}
-
-			particles.get(tempParticleCount1).leapPosition(tempGForce, dt);			
-			tempParticleCount1 += 1;
-
-		}
-
-		//////////////////////////
-
-		//Force calculations, don't need to beware double-counting
-		//For Loop should work, haven't had time to test yet
-		//create gforcecurrent list and populate;
-		//list structure:
-		// 0-1, ... 0-n, 1-0, 1-2, ..., n-(n-1)
-			
-		ArrayList<Vector3D> gforcenew = new ArrayList<>();
-		
-		tempParticleCount1 = 0;
-		
-		for (tempParticleCount1 = 0; tempParticleCount1 > particleCount; tempParticleCount1 += 1){
-			for (tempParticleCount2 = 0; tempParticleCount2 > particleCount; tempParticleCount2 += 1){
-				if (tempParticleCount1 != tempParticleCount2){
-				
-					gforcenew.add(gForce(particles.get(tempParticleCount1),particles.get(tempParticleCount2)));			
-			}	
-		}
-		}
-
-		///////////////////////
-		//test print statement, check if code runs
-		System.out.printf("When I was a lad I served a term as office boy to an attorney's firm");
 
 	}
-	
-	//END OF MAIN METHOD
-
-
 
 
 	//System Static Methods
@@ -120,9 +54,8 @@ public class NBody {
 		Vector3D r = Particle3D.particleSeparation(a,b);
 		double rmag = r.mag();
 		double ma = a.getMass();
-		double mb = b.getMass(); 	
-		double pEnergy = g * ma * mb * (1/rmag);
-		return pEnergy;  	
+		double mb = b.getMass();
+		return -g * ma * mb * (1.0/rmag);
 
 	}
 
@@ -139,11 +72,79 @@ public class NBody {
 		Vector3D gforce = rhat;
 		double ma = a.getMass();
 		double mb = b.getMass();
-		gforce.multScalar(-1*ma*mb*g);
+		gforce.multScalar(-1.0*ma*mb*g);
 		gforce.divScalar(rmag*rmag);
 
 		return gforce;
 
 	}
 
+	public static ArrayList<Vector3D> totalInteractionForces(ArrayList<Particle3D> particles){
+		ArrayList<Vector3D> forces = new ArrayList<>(particles.size());
+
+		// Calculate the force on the i-th particle
+		for(int i=0;i<particles.size();i++){
+			// Total force on i-th particle
+			Vector3D force_i = new Vector3D(0.0, 0.0, 0.0);
+			// Add forces due to all other particles except itself
+			for(int j=0;j<particles.size();j++){
+				if(i != j) {
+					Vector3D.add(force_i, gForce(particles.get(i), particles.get(j)));
+				}
+			}
+			forces.set(i, force_i);
+		}
+
+		return forces;
+	}
+
+	//Return the total energy of the system of particles
+	public static double totalEnergy(ArrayList<Particle3D> particles){
+		double totalKinetic = 0.0;
+		double totalPotential = 0.0;
+
+		//Calculate total kinetic energy
+		for(int i=0;i<particles.size();i++){
+			totalKinetic += particles.get(i).kineticEnergy();
+		}
+
+		//Calculate total potential energy
+		for(int i=0;i<particles.size();i++){
+			for(int j=0;j<i-1;j++){
+				totalPotential+= -1.0 * g * particles.get(i).getMass() * particles.get(j).getMass()
+										/ Particle3D.particleSeparation(particles.get(i), particles.get(j)).mag();
+			}
+		}
+
+		return totalKinetic + totalPotential;
+	}
+
+	//Leap the velocities of an ArrayList of particles using an ArrayList of forces
+	public static void leapVelocities(ArrayList<Particle3D> particles, ArrayList<Vector3D> forces, double dt){
+		for(int i=0;i<particles.size();i++){
+			particles.get(i).leapVelocity(forces.get(i), dt);
+		}
+	}
+
+	//Leap the positions of an ArrayList of particles using an ArrayList of forces
+	public static void leapPositions(ArrayList<Particle3D> particles, ArrayList<Vector3D> forces, double dt){
+		for(int i=0;i<particles.size();i++){
+			particles.get(i).leapPosition(forces.get(i), dt);
+		}
+	}
+
+	//Convenience method for computing the element-wise average of two equally sized ArrayLists
+	public static ArrayList<Vector3D> elementAverage(ArrayList<Vector3D> a, ArrayList<Vector3D> b){
+		if(a.size() == b.size()) {
+			ArrayList<Vector3D> averages = new ArrayList<>();
+			for (int i=0; i<a.size();i++){
+				Vector3D vec = Vector3D.add(a.get(i), b.get(i));
+				vec.divScalar(2.0);
+				averages.set(i, vec);
+			}
+			return averages;
+		} else {
+			return new ArrayList<>();
+		}
+	}
 }
